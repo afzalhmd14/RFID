12f4b139caf39e62cbc04202ada8c9f99968ff07
Raghuram <krish.raghuram@gmail.com>
2016-03-17 00:26:57 +0530

    Modified Reader by Creating a Class

--- src/Constants.py
+++ src/Constants.py
@@ -1,7 +1,7 @@
 #SQL CONSTANTS
 sql_server = "localhost"
 sql_user = "pi"
-sql_pass = ""
+sql_pass = "raspberry"
 sql_db = "IITG"
 dbTable = "students"
 logTable = "logs"
@@ -11,8 +11,8 @@ timeOut = 5 #Time to wait for data from iq
 timeSleep = 5 #Time that the system sleeps after reading a card
 
 #OAUTH Constants
-OAUTH_CLIENT_ID = '647866863318-s4ja5q1s5c0o7r1kes7lsgtbe30kvvkp.apps.googleusercontent.com'
-OAUTH_CLIENT_SECRET = '_kOgmPkPOiXLrZAnYuP913Ug'
+OAUTH_CLIENT_ID = ''#Lookup from Google Developers Console
+OAUTH_CLIENT_SECRET = ''#Lookup from google developers console
 OAUTH_SCOPE = 'https://spreadsheets.google.com/feeds'
 OAUTH_REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 OAUTH_CREDENTIALS_FILE = 'credentials_file'
@@ -21,4 +21,4 @@ OAUTH_CREDENTIALS_FILE = 'credentials_file'
 REGISTRATION_FORM_URI = 
 REGISTRATION_RESPONSES_URI = 
 LOG_FORM_URI = 
-LOG_FORM_RESPONSES = 
+LOG_RESPONSES_URI = 
